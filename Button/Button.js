a2acc632314ee4cc591fd5d27611b47866163f5a
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-05-31 13:58:07 +0530

    Added disabled css in Button Component

--- Button/Button.js
+++ Button/Button.js
@@ -7,6 +7,7 @@ const Button = (props) => {
       type={props.type || 'button'}
       onClick={props.onClick}
       className={`${classes.button} ${props.className}`}
+      disabled={props.disabled}
     >
       {props.children}
     </button>
